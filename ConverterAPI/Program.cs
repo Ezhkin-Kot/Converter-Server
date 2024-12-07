02e505e5d40ffa46e0b9e4794f5e04bcf1c71ec9
Ezhkin-Kot <Ezkot.tokzE@gmail.com>
2024-12-08 00:14:48 +0400

    feat(Program.cs): add connection to db

--- ConverterAPI/Program.cs
+++ ConverterAPI/Program.cs
@@ -1,5 +1,8 @@
+using ConverterAPI;
+using System.Threading.Tasks.Sources;
 using Microsoft.OpenApi.Models;
 using ConverterAPI.DB;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,13 +17,21 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// Get the connection string
+string connectionString = ConfigurationHelper.GetConnectionString("DefaultConnection");
+
+// Connect to the PostgreSQL server
+await using var conn = new NpgsqlConnection(connectionString);
+await conn.OpenAsync();
+
+Console.WriteLine($"The PostgreSQL version: {conn.PostgreSqlVersion}");
+
+//app.UseHttpsRedirection();
 
 app.MapGet("/users", () => User.GetUsers());
+app.MapGet("/users/{id}", (int id) => User.GetUser(id));
 app.MapPost("/users", (UserDb user) => User.CreateUser(user));
 app.MapPut("/users", (UserDb user) => User.UpdateUser(user));
 app.MapDelete("/users/{id}", (int id) => User.RemoveUser(id));
-
-app.Run();
 
 app.Run();
