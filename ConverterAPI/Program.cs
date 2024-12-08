2f65a45ca687f4f93d5f6a716d5242aef94c5261
Ezhkin-Kot <Ezkot.tokzE@gmail.com>
2024-12-08 17:06:23 +0400

    feat(User.cs): add CRUD, but does not work

--- ConverterAPI/Program.cs
+++ ConverterAPI/Program.cs
@@ -2,6 +2,7 @@ using ConverterAPI;
 using System.Threading.Tasks.Sources;
 using Microsoft.OpenApi.Models;
 using ConverterAPI.DB;
+using Microsoft.AspNetCore.Mvc;
 using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,9 +21,9 @@ if (app.Environment.IsDevelopment())
 //app.UseHttpsRedirection();
 
 app.MapGet("/users", () => User.GetUsers());
-app.MapGet("/users/{id}", (int id) => User.GetUser(id));
-app.MapPost("/users", (UserDb user) => User.CreateUser(user));
-app.MapPut("/users", (UserDb user) => User.UpdateUser(user));
-app.MapDelete("/users/{id}", (int id) => User.RemoveUser(id));
+app.MapGet("/users/{id:int}", (int id) => User.GetUser(id));
+app.MapPost("/users", (string newUser) => User.CreateUser(newUser));
+app.MapPut("/users", (string updUser) => User.UpdateUser(updUser));
+app.MapDelete("/users/{id:int}", (int id) => User.RemoveUser(id));
 
 app.Run();
