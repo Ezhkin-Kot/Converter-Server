f02b74a0f9d38cb727767324be75094c9f902d51
Ezhkin-Kot <Ezkot.tokzE@gmail.com>
2024-12-11 01:12:58 +0400

    feat(SessionsDb.cs): start making sessions

--- ConverterAPI/DB/Session/SessionDb.cs
+++ ConverterAPI/DB/Session/SessionDb.cs
@@ -1,30 +1,41 @@
-using Microsoft.EntityFrameworkCore;
-using ConverterAPI.DB.User;
-using Microsoft.AspNetCore.Mvc;
-
 namespace ConverterAPI.DB.Session;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using ConverterAPI.DB.User;
 
 public class SessionDb
 {
     private static readonly string ConnectionString = ConfigurationHelper.GetConnectionString("DefaultConnection");
-
+    
     // Connection to DB
-    private static readonly DbContextOptions<SessionDbContext> Options = new DbContextOptionsBuilder<SessionDbContext>()
+    private static readonly DbContextOptions<ApplicationDbContext> Options = new DbContextOptionsBuilder<ApplicationDbContext>()
         .UseNpgsql(ConnectionString)
         .Options;
     
-    public static async Task<JsonResult> AuthUser(User.User? user)
+    public static async Task<JsonResult> AuthUser(NewUser? newUser)
     {
         var session = new Session();
-        bool isAuthenticated = false;
-        if (user != null)
+        if (newUser != null)
         {
-            await using var context = new SessionDbContext(Options);
-            context.Sessions.Add(session);
-            await context.SaveChangesAsync();
+            await using var applicationDbContext = new ApplicationDbContext(Options);
+            var user = await applicationDbContext.Users.FindAsync(newUser.login);
+            if (user == null)
+            {
+                return new JsonResult(new { success = false, error = "Invalid login or password" });
+            }
+            if (PasswordManager.VerifyHashedPassword(user.password, newUser.password))
+            {
+                session.userid = user.id;
+                session.datetime = DateTime.Now;
+            
+                await applicationDbContext.Sessions.AddAsync(session);
+                await applicationDbContext.SaveChangesAsync();
 
-            return new JsonResult("Session started");
+                return new JsonResult("Session started");
+            }
+            else return new JsonResult(new { success = false, error = "Invalid login or password" });
         }
-        else return new JsonResult("Unable to start session");
+        else return new JsonResult("Incorrect query");
     }
 }
