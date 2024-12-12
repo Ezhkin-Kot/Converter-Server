0548234aaa71fa23a11ac31f762f005a60ad8eef
Ezhkin-Kot <Ezkot.tokzE@gmail.com>
2024-12-12 18:48:26 +0400

    refactor(Sessions.cs): rename amount field

--- ConverterAPI/DB/Session/Session.cs
+++ ConverterAPI/DB/Session/Session.cs
@@ -5,6 +5,6 @@ public record Session()
     public int sessionid { get; set; }
     public int userid { get; set; }
     public DateTime datetime { get; set; }
-    public int conversionsamount { get; set; }
+    public int amount { get; set; }
     public bool active { get; set; }
 }
