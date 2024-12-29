46582fdab65adc49addf225ec350c8ff5cc94e6f
Ezhkin-Kot <Ezkot.tokzE@gmail.com>
2024-12-29 12:57:14 +0400

    refactor(Program.cs): separate users and sessions methods

--- ConverterAPI/Program.cs
+++ ConverterAPI/Program.cs
@@ -30,15 +30,16 @@ app.UseRouting();
 
 app.MapGet("/users", () => UserDb.GetUsers());
 app.MapGet("/users/{id:int}", (int id) => UserDb.GetUserById(id));
-app.MapGet("/sessions", () => SessionDb.GetSessions());
-app.MapGet("/sessions/{userid:int}", (int userid) => SessionDb.GetSessionByUserId(userid));
 app.MapPost("/users/reg", ([FromBody] NewUser newUser) => UserDb.CreateUser(newUser));
-app.MapPost("/sessions/auth", ([FromBody] NewUser newUser) => SessionDb.AuthUser(newUser));
 app.MapPut("/users", ([FromBody] User updUser) => UserDb.UpdateUser(updUser));
 app.MapPut("/users/prem/{id:int}/{premium:bool}", (int id, bool premium) => UserDb.ChangePremium(id, premium));
+app.MapDelete("/users/{id:int}", (int id) => UserDb.DeleteUser(id));
+
+app.MapGet("/sessions", () => SessionDb.GetSessions());
+app.MapGet("/sessions/{userid:int}", (int userid) => SessionDb.GetSessionByUserId(userid));
+app.MapPost("/sessions/auth", ([FromBody] NewUser newUser) => SessionDb.AuthUser(newUser));
 app.MapPut("/sessions/close/{userid:int}", (int userid) => SessionDb.CloseSession(userid));
 app.MapPut("/sessions/upd/{userid:int}", (int userid) => SessionDb.UpdAmount(userid));
-app.MapDelete("/users/{id:int}", (int id) => UserDb.DeleteUser(id));
 app.MapDelete("/sessions/{userid:int}", (int userid) => SessionDb.DeleteSession(userid));
 
 app.UseCors("AllowAll");
