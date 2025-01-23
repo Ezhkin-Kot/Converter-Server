f54e786767bbc537235c9cdb5d87e44816cab0d0
Ezhkin-Kot <Ezkot.tokzE@gmail.com>
2025-01-24 01:35:11 +0400

    feat! transform SessionsDb to controller SessionsController

--- ConverterAPI/Controllers/SessionsController.cs
+++ ConverterAPI/Controllers/SessionsController.cs
@@ -4,121 +4,147 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ConverterAPI.Controllers;
 
-public static class SessionDb
+[ApiController]
+[Route("[controller]")]
+public class SessionsController(ApplicationDbContext context) : ControllerBase
 {
-    private static readonly string ConnectionString = ConfigurationHelper.GetConnectionString("DefaultConnection");
-    
-    // Connection to DB
-    private static readonly DbContextOptions<ApplicationDbContext> Options = new DbContextOptionsBuilder<ApplicationDbContext>()
-        .UseNpgsql(ConnectionString)
-        .Options;
+    [HttpGet]
+    public async Task<IActionResult> GetSessions()
+    {
+        var sessions = await context.Sessions.ToListAsync();
 
-    public static async Task<List<Models.Session>> GetSessions()
-    {
-        await using var context = new ApplicationDbContext(Options);
-        return await context.Sessions.ToListAsync();
-    }
-    public static async Task<Models.Session?> GetSessionByUserId(int userid)
-    {
-        await using var context = new ApplicationDbContext(Options);
-        return await context.Sessions.OrderBy(s => s.sessionid).LastOrDefaultAsync(s => s.userid == userid);
-    }
-    
-    public static async Task<JsonResult> AuthUser(NewUser? newUser)
-    {
-        if (newUser != null)
+        if (sessions.Count == 0)
         {
-            await using var context = new ApplicationDbContext(Options);
-            var user = await context.Users.FirstOrDefaultAsync(u => u.login == newUser.login);
-            if (user != null && PasswordManager.VerifyPassword(newUser.password, user.password, user.salt))
-            {
-                var session = await context.Sessions.OrderBy(s => s.sessionid).LastOrDefaultAsync(s => s.userid == user.id);
-                if (session is { active: true })
-                {
-                    return new JsonResult(new { success = true, user });
-                }
-                else
-                {
-                    session = new Models.Session();
-                    session.userid = user.id;
-                    session.datetime = DateTime.UtcNow;
-                    session.amount = 5;
-                    session.active = true;
-            
-                    await context.Sessions.AddAsync(session);
-                    await context.SaveChangesAsync();
-
-                    return new JsonResult(new { success = true, user });
-                }
-            }
-            else return new JsonResult(new { success = false, error = "Invalid login or password" });
+            return NotFound(new { message = "No sessions found." });
         }
-        else return new JsonResult(new { success = false, error = "Incorrect query"});
+        
+        return Ok(new { sessions });
     }
     
-    public static async Task<JsonResult> UpdAmount(int userid)
+    [HttpGet("{userid:int}")]
+    public async Task<IActionResult> GetSessionByUserId(int userid)
     {
-        await using var context = new ApplicationDbContext(Options);
-        var user = await context.Users.FirstOrDefaultAsync(u => u.id == userid);
-        var session = await context.Sessions.OrderBy(s => s.sessionid).LastOrDefaultAsync(s => s.userid == userid);
-
-        if (session != null && user != null)
-        {
-            if (session.active)
-            {
-                if (user.premium)
-                {
-                    session.amount = 5;
-                    return new JsonResult(new { session.amount, message = "Amount unlimited" });
-                }
-                if (session.amount <= 0)
-                {
-                    return new JsonResult(new { session.amount, message = "Amount expire" });
-                }
-                
-                session.amount--;
-                context.Sessions.Update(session);
-                await context.SaveChangesAsync();
-                
-                return new JsonResult(new { session.amount, message = "Amount updated" });
-            }
-            else return new JsonResult(new { success = false, error = "Session isn't active" });
-        }
-        else return new JsonResult(new { success = false, error = "Session not found" });
-    }
-    
-    public static async Task<JsonResult> CloseSession(int userid)
-    {
-        await using var context = new ApplicationDbContext(Options);
-        var session = await context.Sessions.OrderBy(s => s.sessionid).LastOrDefaultAsync(s => s.userid == userid);
-
-        if (session != null)
-        {
-            if (session.active)
-            {
-                session.active = false;
-                context.Sessions.Update(session);
-                await context.SaveChangesAsync();
-
-                return new JsonResult(new { success = true, message = "Session closed" });
-            }
-            else return new JsonResult(new { success = false, error = "Session is already closed" });
-        }
-        else return new JsonResult(new { success = false, error = "Session not found"});
-    }
-    
-    public static async Task<JsonResult> DeleteSession(int userid)
-    {
-        await using var context = new ApplicationDbContext(Options);
-        var session = await context.Sessions.FirstOrDefaultAsync(s => s.userid == userid);
+        var session = await context.Sessions.OrderBy(s => s.sessionid)
+            .LastOrDefaultAsync(s => s.userid == userid);
+        
         if (session == null)
         {
-            return new JsonResult(new {success = false, error = "Session not found" });
+            return NotFound(new { message = "No session found." });
+        }
+        
+        return Ok(new { session });
+    }
+    
+    [HttpPost("auth")]
+    public async Task<IActionResult> AuthUser([FromBody] NewUser? newUser)
+    {
+        if (newUser == null)
+        {
+            return BadRequest(new { message = "Invalid request" });
+        }
+        
+        var user = await context.Users.FirstOrDefaultAsync(u => u.login == newUser.login);
+
+        if (user == null || !PasswordManager.VerifyPassword(newUser.password, user.password, user.salt))
+        {
+            return new JsonResult(new { success = false, error = "Invalid login or password" });
+        }
+        
+        var session = await context.Sessions.OrderBy(s => s.sessionid)
+            .LastOrDefaultAsync(s => s.userid == user.id);
+        
+        if (session is { active: true })
+        {
+            return Ok(new { session, message = "Connected to existing session" });
+        }
+        
+        session = new Session
+        {
+            userid = user.id,
+            datetime = DateTime.UtcNow,
+            amount = 5,
+            active = true
+        };
+
+        await context.Sessions.AddAsync(session);
+        await context.SaveChangesAsync();
+
+        return Ok(new { session, message = "New session started" });
+    }
+    
+    [HttpPatch("upd/{userid:int}")]
+    public async Task<IActionResult> UpdAmount(int userid)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(u => u.id == userid);
+        var session = await context.Sessions.OrderBy(s => s.sessionid)
+            .LastOrDefaultAsync(s => s.userid == userid);
+
+        if (user == null)
+        {
+            return BadRequest(new { message = "User does not exist." });
+        }
+        if (session == null)
+        {
+            return NotFound(new { message = "No session found." });
+        }
+        if (!session.active)
+        {
+            return BadRequest(new { message = "Session is not active." });
+        }
+        
+        if (user.premium)
+        {
+            session.amount = 5;
+            return Ok(new { session.amount, message = "Amount unlimited." });
+        }
+        
+        if (session.amount <= 0)
+        {
+            return StatusCode(403, new { message = "Amount expired." });
+        }
+
+        session.amount--;
+        context.Sessions.Update(session);
+        await context.SaveChangesAsync();
+
+        return Ok(new { session.amount, message = "Amount updated" });
+    }
+    
+    [HttpPatch("close/{userid:int}")]
+    public async Task<IActionResult> CloseSession(int userid)
+    {
+        var session = await context.Sessions.OrderBy(s => s.sessionid)
+            .LastOrDefaultAsync(s => s.userid == userid);
+
+        if (session == null)
+        {
+            return NotFound(new { message = "Session does not exist." });
+        }
+        if (!session.active)
+        {
+            return BadRequest(new { message = "Session is already closed." });
+        }
+        
+        session.active = false;
+        context.Sessions.Update(session);
+        await context.SaveChangesAsync();
+
+        return Ok(new { message = "Session closed." });
+    }
+    
+    [HttpDelete("{userid:int}")]
+    public async Task<IActionResult> DeleteSession(int userid)
+    {
+        var session = await context.Sessions.FirstOrDefaultAsync(s => s.userid == userid);
+        
+        if (session == null)
+        {
+            return NotFound(new { message = "No session found." });
         }
 
         context.Sessions.Remove(session);
         await context.SaveChangesAsync();
 
-        return new JsonResult(new { success = true, message = "Session successfully deleted"});
+        return Ok(new { message = "Session successfully deleted." });
     }
 }
