b321584eefade6a4392866a357bdac4badff2a2e
Ezhkin-Kot <Ezkot.tokzE@gmail.com>
2025-02-07 00:58:35 +0400

    refactoring: delete unused code for old sessions

--- ConverterAPI/Services/ApplicationDbContext.cs
+++ ConverterAPI/Services/ApplicationDbContext.cs
@@ -5,11 +5,9 @@ namespace ConverterAPI.Services;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
     public DbSet<Models.User> Users { get; set; }
-    public DbSet<Models.Session> Sessions { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Models.User>().ToTable("users");
-        modelBuilder.Entity<Models.Session>().ToTable("sessions");
     }
 }
