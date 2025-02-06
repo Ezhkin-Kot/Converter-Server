49de2957d019299a8ea8ee6e72a887334fc70f29
Ezhkin-Kot <Ezkot.tokzE@gmail.com>
2025-02-07 00:58:35 +0400

    refactoring: delete unused code for old sessions

--- ConverterAPI/Models/Session.cs
+++ ConverterAPI/Models/Session.cs
@@ -2,7 +2,6 @@ namespace ConverterAPI.Models;
 
 public record Session()
 {
-    public int sessionid { get; set; }
     public int userid { get; set; }
     public DateTime datetime { get; set; }
     public int amount { get; set; }
